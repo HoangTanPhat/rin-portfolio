83cc0d9d72b45c321f164c9d62f7340eb7cfa383
Hoang Tan Phat <hoangtanphat97.xm@gmail.com>
2025-03-01 00:34:52 +0700

    fix: add link to branding section on homepage

--- components/home/BrandingSection.tsx
+++ components/home/BrandingSection.tsx
@@ -12,10 +12,10 @@ export default function BrandingSection() {
         <h2 className='text-2xl text-textSecondary my-8'>Branding</h2>
         <div className="grid grid-cols-3 gap-3 w-full mb-3">
             <div className='relative aspect-square'>
-                <ImageWithText src={pulseInnovatesBranding} title="BRAND: PULSE INNOVATES" href="#" />
+                <ImageWithText src={pulseInnovatesBranding} title="BRAND: PULSE INNOVATES" href="/branding/pulse-innovates" />
             </div>
             <div className='relative aspect-square'>
-                <ImageWithText src={dangKhoiBranding} title="BRAND: DANG KHOI TRAVEL" href="#" />
+                <ImageWithText src={dangKhoiBranding} title="BRAND: DANG KHOI TRAVEL" href="/branding/dang-khoi-travel" />
             </div>
             <div className='relative aspect-square'>
                 <ImageWithText src={xynioBranding} title="BRAND: XYNIO" href="#" />
