416d3ffa140c0bab3abd0660e22afa99dc2e42da
Hoang Tan Phat <hoangtanphat97.xm@gmail.com>
2025-03-15 21:03:17 +0700

    feat: Add GTM and GA4

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import LayoutContainer from "@/components/layout/LayoutContainer";
 import { Work_Sans } from "next/font/google";
 import type { Metadata } from "next";
 import { ViewTransitions } from "next-view-transitions";
+import Script from 'next/script'
 
 import "./globals.css";
 
@@ -22,10 +23,21 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const gtmId = process.env.NEXT_PUBLIC_GTM_ID; // Replace with your GTM ID or use process.env.NEXT_PUBLIC_GTM_ID
+
   return (
     <ViewTransitions>
       <html lang="en" className={workSans.className}>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <head>
+          <Script id='gtm-script' strategy="afterInteractive">
+            {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${gtmId}');`}
+          </Script>
+        </head>
+       <link rel="icon" href="/favicon.ico" sizes="any" />
         <body
           className={workSans.className}
           style={{
@@ -36,6 +48,14 @@ export default function RootLayout({
             backgroundColor: "#fffff0",
           }}
         >
+         <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${gtmId}`}
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
           <AllProviders>
             <LayoutContainer>{children}</LayoutContainer>
           </AllProviders>
